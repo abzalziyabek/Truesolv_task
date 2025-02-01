75ad1964605e31b2c5cae843d86a4f499c941f97
abzalziyabek <78420174+abzalziyabek@users.noreply.github.com>
2025-02-02 03:11:21 +0500

    Delete force-app/main/default directory

--- force-app/main/default/lwc/orderManagementButton/orderManagementButton.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-import { NavigationMixin } from 'lightning/navigation';
-
-export default class OrderManagementButton extends NavigationMixin(LightningElement) {
-    handleOpenOrderManagement() {
-        // Open Order Management in a new tab
-        window.open('/lightning/n/Order_Management', '_blank');
-    }
-}
